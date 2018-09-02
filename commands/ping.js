22c7ad8a5a0915fe0a346783836a1aa1645b425f
Cappuchino <38139192+Crkza@users.noreply.github.com>
2018-09-02 16:36:12 +0700

    Uses await now

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const senko = new Discord.Client({disableEveryone: true});
 exports.run = async (senko, message, args) => {
-    if (!senko.ping) return message.channel.send("Latency: Literally empty, Thanks [insert online hosting service here]."); 
+    if (!senko.ping) await message.channel.send("Latency: Literally empty, Thanks [insert online hosting service here]."); 
     else{
         const ping = math.round(senko.ping) 
-        return message.channel.send(`Latency: :vertical_traffic_light:  ${ping} milliseconds`)
+        await message.channel.send(`Latency: :vertical_traffic_light:  ${ping} milliseconds`)
     }
     }
     
