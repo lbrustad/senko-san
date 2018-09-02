e63b333ff1fc26d2490aa0dd7940c7e4d7d82361
Cappuchino <crkza1134@gmail.com>
2018-09-02 18:57:37 +0700

    aaaaaa

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const senko = new Discord.Client({disableEveryone: true});
 module.exports.run = async(senko, message, args, con) => {
     let str = ''
-    client.commands.array().forEach( (i,v) =>{
+    senko.commands.array().forEach( (i,v) =>{
             str += $`{i.help.name}\n`
     })
     msg.channel.send(str)
