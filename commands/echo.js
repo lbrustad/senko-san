59fbefcc463fe93c3ff44627d8617dde43b2cac1
Crkza <crkza1134@gmail.com>
2018-09-07 08:11:54 +0700

    more error fixes

--- commands/echo.js
+++ commands/echo.js
@@ -5,8 +5,8 @@ exports.run = async (senko, message, args) => {
     const sayMessage = args.join(" ");
     if(!sayMessage){
         await message.channel.send("Usage: ```echo {message}```")};        
-        message.delete().catch(O_o=>{}); 
-        message.channel.send(sayMessage);
+        else{message.delete().catch(O_o=>{}); 
+        message.channel.send(sayMessage)};
 }
 
 exports.help = {
