334f33a713e19f67646ee403fdb857a06fac0bcb
Cappuchino <38139192+Crkza@users.noreply.github.com>
2018-09-08 22:57:52 +0700

    Update ready.js

--- events/general/ready.js
+++ events/general/ready.js
@@ -1,6 +1,6 @@
 const pack = require('../../package.json');
 
-exports.run = async senko => {
+exports.run = async (senko) => {
   senko.guilds.forEach(guild => {
     senko.settings.ensure(guild.id, { prefix: 'sk-', aliases: {} });
   });
