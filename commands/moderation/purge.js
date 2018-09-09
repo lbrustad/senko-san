2e4b558bbf15da3691412c58ec796134a01b1757
Cappuchino <crkza1134@gmail.com>
2018-09-09 18:37:01 +0700

    updated error msgs

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
         const deleteCount = parseInt(args[0], 10);
         
         if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-          return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+          return message.reply("`Error 411`:Please provide a number between 2 and 100 for the number of messages to delete");
         
         const fetched = await message.channel.fetchMessages({limit: deleteCount});
         message.channel.bulkDelete(fetched)
