6facce70caaf815ed62f6917b898c9b3fda3b5c5
Cappuchino <crkza1134@gmail.com>
2018-09-10 18:57:01 +0700

    hardcoded name lol

--- commands/utilities/botinfo.js
+++ commands/utilities/botinfo.js
@@ -1,16 +1,14 @@
 const Discord = require("discord.js")
-const pack = require(".../package.json")
-const config = require(".../config.json")
 exports.run = async (senko, message, args) => {
     let pfp = senko.user.displayAvatarURL;
     let embed = new Discord.RichEmbed()
     .setTitle(`About ${senko.user.username} `)
     .setAuthor(senko.user.username, senko.user.avatarURL)
-    .setDescription(`Version ${pack.version} `)
+    .setDescription(`Version 0.1.4 Alpha `)
     .setColor("#f28a30")
     .setThumbnail(pfp)
-    .addField("Author", `<@!${config.owner}>`)
-    .addField("Prefix", `\`${config.prefix}\``)
+    .addField("Author", `Cappuchino`)
+    .addField("Prefix", `\`sk-\``)
     .addField("Server Link:", "discord.gg/KfFTecu")
     await message.channel.send(embed);
 }
