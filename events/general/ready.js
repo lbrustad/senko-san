9549cd68de37c296c2e4a960784a0ee03253fdca
Cappuchino <crkza1134@gmail.com>
2018-09-14 23:45:24 +0700

    ok

--- events/general/ready.js
+++ events/general/ready.js
@@ -10,7 +10,6 @@ exports.run = async senko => {
   console.log(`-----------------------------------`);
   console.log(`Version ${pack.version}`);
   console.log(`Made by ${pack.author}`);
-  console.log(`Incomplete Alpha`);
   console.log(`[${senko.user.username}] is now online.`);
   console.log(`Bot is on ${senko.guilds.size} servers.`);
   await senko.user.setActivity("w/ Nakano");
